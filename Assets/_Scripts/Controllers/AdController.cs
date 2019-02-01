ae4047337619af856587dfde601eb9c1f0d9311b
Golden4 <g.axe@list.ru>
2019-02-01 05:16:03 +0500

    WitoutAppodeal

--- Assets/_Scripts/Controllers/AdController.cs
+++ Assets/_Scripts/Controllers/AdController.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GoogleMobileAds.Api;
+
+/*using GoogleMobileAds.Api;
 using AppodealAds.Unity.Api;
-using AppodealAds.Unity.Common;
+using AppodealAds.Unity.Common;*/
 
 public class AdController : MonoBehaviour {
 
@@ -15,13 +16,13 @@ public class AdController : MonoBehaviour {
 	void Start ()
 	{
 		Ins = this;
-		Appodeal.initialize (AppID, Appodeal.REWARDED_VIDEO | Appodeal.NON_SKIPPABLE_VIDEO | Appodeal.INTERSTITIAL);
+		/*Appodeal.initialize (AppID, Appodeal.REWARDED_VIDEO | Appodeal.NON_SKIPPABLE_VIDEO | Appodeal.INTERSTITIAL);*/
 	}
 
 	public void ShowAD ()
 	{
-		if (Appodeal.isLoaded (Appodeal.INTERSTITIAL)) {
+		/*if (Appodeal.isLoaded (Appodeal.INTERSTITIAL)) {
 			Appodeal.show (Appodeal.INTERSTITIAL);
-		}
+		}*/
 	}
 }
--- Assets/_Scripts/Screens/GameOverScreen.cs
+++ Assets/_Scripts/Screens/GameOverScreen.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GoogleMobileAds.Api;
 using UnityEngine.UI;
 
 public class GameOverScreen : ScreenBase {
